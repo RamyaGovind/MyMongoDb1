a3c82d2925ee960a08550e7b55981c139a9b0a6a
Ramya <ramya@voonik.com>
2015-08-05 18:02:13 +0530

    short name change

--- nodeapp/recipes/default.rb
+++ nodeapp/recipes/default.rb
@@ -1,10 +1,11 @@
-deploy = node[:deploy]['Analytics']
+deploy = node[:deploy]['voonik_analytics']
 
 script "setup_test_app" do
   interpreter "bash"
   user "root"
-  cwd "#{deploy[:deploy_to]}/current/app-test"
+  cwd "#{deploy[:deploy_to]}/current"
   code <<-EOH
     npm install -d
+    
   EOH
 end
