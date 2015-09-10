52914c98955f4e037ad888d8d5fe3dc94702ecda
RamyaGovind <ramya@voonik.com>
2015-09-10 12:47:27 +0530

    Update default.rb

--- mongodb-mysite/recipes/default.rb
+++ mongodb-mysite/recipes/default.rb
@@ -1,3 +1,3 @@
-node.normal[:mongodb][:config][:bind_ip] = "127.0.0.1,#{node[:opsworks][:instance][:private_ip]},0.0.0.0"
+node.normal[:mongodb][:config][:bind_ip] = "0.0.0.0"
 include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::default"
